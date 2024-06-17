fc59a2eb71ae0664272c868b070f83e64561b795
togatoga <hitoshitogasaki@gmail.com>
2024-06-17 12:17:47 +0900

    Impl subcommand

--- src/bin/kanpyo.rs
+++ src/bin/kanpyo.rs
@@ -1,36 +1,63 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use dict::dict;
 use kanpyo::{lattice::node::BOS_EOS_ID, tokenizer::Tokenzier};
-use std::{io::Read, path::PathBuf};
+use std::{path::PathBuf};
 
 #[derive(Parser)]
 #[command(name = "kanpyo", about = "Japanese Morphological Analyzer", version = "0.1", long_about=None)]
 struct KanpyoCommand {
-    /// Input text to analyze [default: stdin]
-    #[arg(index = 1)]
-    input: Option<String>,
-    /// Dictionary file
-    #[arg(short, long, default_value = "ipa.dict")]
-    dict: PathBuf,
+    /// Subcommand
+    #[command(subcommand)]
+    subcommand: Option<SubCommand>,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommand {
+    /// Tokenize input text
+    Tokenize {
+        /// Input text to analyze [default: stdin]
+        #[arg(index = 1)]
+        input: Option<String>,
+        /// Dictionary file
+        #[arg(short, long, default_value = "dict/ipa.dict")]
+        dict: PathBuf,
+    },
+    Visualize,
 }
 
 impl KanpyoCommand {
-    fn run(&self) {
+    fn tokenize(input: Option<String>, dict: PathBuf) {
         let mut reader =
-            std::io::BufReader::new(std::fs::File::open(&self.dict).expect("failed to open dict"));
+            std::io::BufReader::new(std::fs::File::open(dict).expect("failed to open dict"));
         let tokenzier = Tokenzier::new(dict::Dict::load(&mut reader).expect("failed to load dict"));
-        let tokens = match &self.input {
-            Some(text) => tokenzier.tokenize(text),
-            None => {
-                let mut buf = String::new();
-                std::io::stdin()
-                    .read_to_string(&mut buf)
-                    .expect("failed to read from stdin");
-                // trim last newline
-                tokenzier.tokenize(buf.trim_end())
+        loop {
+            let tokens = match &input {
+                Some(text) => tokenzier.tokenize(text),
+                None => {
+                    let mut buf = String::new();
+                    std::io::stdin()
+                        .read_line(&mut buf)
+                        .expect("failed to read from stdin");
+                    // trim last newline
+                    tokenzier.tokenize(buf.trim_end())
+                }
+            };
+            print_tokens(tokens, &tokenzier.dict);
+        }
+    }
+    fn run(&self) {
+        match &self.subcommand {
+            Some(SubCommand::Tokenize { input, dict }) => {
+                KanpyoCommand::tokenize(input.clone(), dict.clone());
             }
-        };
-        print_tokens(tokens, &tokenzier.dict);
+            Some(SubCommand::Visualize) => {
+                println!("Visualize");
+            }
+            None => {
+                let dict = PathBuf::from("dict/ipa.dict");
+                KanpyoCommand::tokenize(None, dict);
+            }
+        }
     }
 }
 
